2514d0c8252f5cdc987f724161b03263362032dc
TanujKS <tanujsiripurapu@gmail.com>
2023-01-28 17:17:41 -0800

    Working call accept/respond

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,70 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const { defineString } = require('firebase-functions/params');
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const { Configuration, OpenAIApi} = require("openai");
+const OPENAI_KEY = defineString("OPENAI_KEY")
 admin.initializeApp();
 
-const accountSid = defineString('accountSid');
-const authToken = defineString('authToken');
-const client = require('twilio')(accountSid, authToken);
-
-exports.call = functions.https.onRequest((req, res) => {
-  client.calls
-        .create({
-           twiml: '<Response><Say>This is an automated ZeroResponder alert. A patient </Say></Response>',
-           to: '+14083103927',
-           from: '+14083378528'
-         })
-        .then(call => console.log(call.sid));
-
-  res.json("ok")
+const configuration = new Configuration({
+  apiKey: OPENAI_KEY,
 })
 
+async function getData() {
+  const victim_data = await admin.firestore().collection("emergencies").doc("neelsdying").data()
+  return victim_data
+}
+
+
+exports.test = functions.https.onRequest(async(req, res) => {
+  console.log(OPENAI_KEY.value())
+  const openai = new OpenAIApi(configuration);
+
+  const question = "How old is the patient?"
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Imagine you are reporting an emergency to 911 regarding a patient. The patient is 27 years old, male, cholesterol of 407, and 130 fasting blood sugar. The operator asks you, " + question + " How do you respond as concisely as possible?",
+  })
+
+
+  res.json((completion.data.choices[0].text));
+})
+
+
+exports.call = functions.https.onRequest(async(req, res) => {
+  const resp = new VoiceResponse()
+  const gather = resp.gather({
+      input: "speech",
+      timeout: 5,
+      action: "/gather"
+    })
+
+  gather.say("Please say your question")
+
+  resp.redirect("/call")
+
+  res.type('text/xml')
+  res.send(resp.toString())
+
+})
+
+exports.gather = functions.https.onRequest(async(req, res) => {
+  const resp = new VoiceResponse()
+
+  console.log("RECEVIED QUESTION")
+  console.log(req.body.SpeechResult)
+  const doc_ref = await admin.firestore().collection("emergencies").doc("neelsdying")
+  doc_ref.update({
+    response: req.body.SpeechResult,
+  })
+
+  resp.redirect("/call")
+
+  res.type('text/xml')
+  res.send(resp.toString())
+
+})
 
 exports.alertResponders = functions.firestore
   .document("emergencies/{victimId}")
